a4f949c30bc07cda8e71ce6a669cc77b2577d51a
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-18 16:33:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,7 +44,9 @@ def monopoly_with_fourth_tier {
     :pieces => 4,
     :names => {
       :reading_railroad => {},
-      :pennsylvania_railroad => {},
+      :pennsylvania_railroad => {
+        "mortgage value" => "$100"
+      },
       :b_and_o_railroad => {},
       :shortline => {}
     },
